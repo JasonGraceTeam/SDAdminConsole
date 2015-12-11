8d1317b9403b55f373ae63bb265159ed5c85331f
VishwasHK <vishwa.vijay143@gmail.com>
2015-12-11 18:39:25 +0530

    Working Angualar JS Project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sdadminConsoleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sdadminConsoleApp
+ */
+angular.module('sdadminConsoleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
